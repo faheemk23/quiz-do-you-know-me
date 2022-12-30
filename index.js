f5b1decab3b99976efba11424d8d957f4652a697
faheemk23 <faheemwork23@gmail.com>
2022-12-30 15:58:44 +0530

    feat: welcome message

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var readlineSync=require('readline-sync')
 
 var score=0
+
+// welcome function
+
+function welcome() {
+    var usrName = readlineSync.question("What's your name? ")
+    console.log("Hello " + usrName + ", welcome to DO YOU KNOW FAHEEM!")
+    console.log("----------------------------------------")
+    }
+
+console.log('a')
