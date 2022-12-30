78a46a538dedad801ce343a9186a713b7c84c9d7
faheemk23 <faheemwork23@gmail.com>
2022-12-30 16:01:52 +0530

    feat: game underline process

--- index.js
+++ index.js
@@ -8,5 +8,22 @@ function welcome() {
     var usrName = readlineSync.question("What's your name? ")
     console.log("Hello " + usrName + ", welcome to DO YOU KNOW FAHEEM!")
     console.log("----------------------------------------")
-    }
+}
 
+    // play function
+
+function play(question, answer) {
+
+    var usrAns = readlineSync.question(question)
+  
+    if (usrAns.toUpperCase() === answer.toUpperCase()) {
+      console.log("Right!")
+  
+      score++
+    } else {
+      console.log("Wrong")
+    }
+    console.log("Current score: " + score)
+    console.log("----------------------------------------")
+}
+  
